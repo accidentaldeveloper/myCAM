2ffe789e7e56435e73d3537607d02fe9fa3e3915
Michael Richardson <probackpacker@gmail.com>
2016-04-09 10:58:24 -0400

    Add project files.

---
+++ myCAM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(myCAM.Startup))]
+namespace myCAM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
